e5aa19623e6b287a7cc8dd684f88d9eaf3c5b6c0
MOS2025 <michealose@gmail.com>
2026-06-19 12:27:27 +0100

    Created urls.py file and created template dir for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, blog initial test!")
+#def my_blog(request):
+#    return HttpResponse("Hello, blog initial test!")
